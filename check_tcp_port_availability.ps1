47e4105bec0aaf28b2b17a08a36cf71bc345c896
AM <anvar.minulin@gmail.com>
2019-08-23 23:27:40 +0300

    fix variables names

--- check_tcp_port_availability.ps1
+++ check_tcp_port_availability.ps1
@@ -1,8 +1,8 @@
 ﻿$Ipaddress= Read-Host "Enter the IP address:"
 $Port= Read-host "Enter the port number to access:"
 
-$t = New-Object Net.Sockets.TcpClient
-$t.Connect($Ipaddress,$Port)
+$connection = New-Object Net.Sockets.TcpClient
+$connection.Connect($Ipaddress,$Port)
     if($t.Connected)
     {
         "Port $Port is open"
