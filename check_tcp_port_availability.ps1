ce68faa2ccdc5d37b72553ec105178223a6c69c7
AM <anvar.minulin@gmail.com>
2019-08-23 23:24:30 +0300

    check tcp port availability

---
+++ check_tcp_port_availability.ps1
@@ -0,0 +1,13 @@
+﻿$Ipaddress= Read-Host "Enter the IP address:"
+$Port= Read-host "Enter the port number to access:"
+
+$t = New-Object Net.Sockets.TcpClient
+$t.Connect($Ipaddress,$Port)
+    if($t.Connected)
+    {
+        "Port $Port is open"
+    }
+    else
+    {
+        "Port $Port is closed"
+    }
